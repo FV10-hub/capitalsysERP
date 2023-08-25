9e2c5984afd369172aa40ebbce69f391e5826693
fvazquez10 <fernando.vazquez@itti.digital>
2023-08-25 15:15:58 -0400

    jj

---
+++ capitalsys-data/src/test/java/py/com/capitalsys/data/AppTest.java
@@ -0,0 +1,20 @@
+package py.com.capitalsys.data;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
